139106fee0939fe3d8b2a4a1103e4ed2f0671adf
Eduard Ghenea <87914375+Edi013@users.noreply.github.com>
2024-01-23 17:47:58 +0200

    First problem implementation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,6 +10,9 @@ int main()
     ifstream inFile("HandsOn-Input.txt");
 
     int no_problems, no_doctors;
+    vector<pair<string, string>> cases;
+    vector<pair<string, string>> doctors;
+    vector<pair<string, string>> result;
     string name, speciality;
     
     inFile >> no_problems;
@@ -17,6 +21,8 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
+
+        cases.emplace_back(name, speciality);
         cout << name << ' ' << speciality << '\n';
     }
 
@@ -26,8 +32,28 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
+
+        doctors.emplace_back(name, speciality);
         cout << name << ' ' << speciality << '\n';
+    }
+
+    for (int i = 0; i < no_problems; i++) {
+        auto problem = cases[i];
+
+        for (int j = 0; j < doctors.size(); j++) {
+            if (doctors[j].second == problem.second) {
+                result.emplace_back(doctors[j].first, problem.first);
+                doctors.erase(doctors.begin()+j );
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i].first<< ' ' << result[i].second << '\n';
     }
 
     return 0;
 }
+
