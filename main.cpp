0e600a30c3d8f83b6df09ddbe93eff347cca7a19
Eduard Ghenea <87914375+Edi013@users.noreply.github.com>
2024-02-06 17:55:31 +0200

    work in progress # 3

--- main.cpp
+++ main.cpp
@@ -4,30 +4,51 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <set>
+#include <map>
 
 using namespace std;
 
 struct Case {
     string name, speciality;
-    int priority, duration;
+    int arrival_time, priority, duration;
     bool reviewd = false;
 };
 
 struct Doctor {
-    string name, speciality;
-    std::queue<Case> queue;
+    string name;
+    set<string> specialities;
+    queue<Case> queue;
     int remainingTime = 8;
 };
 
-void matchSpeciality(Case& currentCase, vector<Doctor>& doctors) {
-    for_each(doctors.begin(), doctors.end(), [&currentCase](auto& currentDoctor){
-        if (currentDoctor.speciality == currentCase.speciality && currentDoctor.remainingTime - currentCase.duration >= 0 &&
-            currentCase.reviewd == false) {
+struct Staff {
+    // specialitati - doctori
+    map<string, vector<Doctor>> doctorsBySpecialities;
+};
+
+void matchSpeciality(Case& currentCase, vector<Doctor>& doctors, Staff staff) {
+    /*for_each(doctors.begin(), doctors.end(), [&currentCase](auto& currentDoctor){
+        if (currentDoctor.specialities.contains(currentCase.speciality) &&
+            currentDoctor.remainingTime - currentCase.duration >= 0 &&
+            currentCase.reviewd == false) 
+        {
             currentDoctor.queue.push(currentCase);
             currentDoctor.remainingTime -= currentCase.duration;
             currentCase.reviewd = true;
         }
-    });
+    });*/
+    
+    for (auto doctor : staff.doctorsBySpecialities[currentCase.speciality]) {
+        if (doctor.remainingTime - currentCase.duration >= 0 &&
+            currentCase.reviewd == false)
+        {
+            doctor.queue.push(currentCase);
+            doctor.remainingTime -= currentCase.duration;
+            currentCase.reviewd = true;
+        }
+    }
+
 }
 
 int main()
@@ -37,8 +58,10 @@ int main()
     int no_problems, no_doctors;
     vector<Case> cases;
     vector<Doctor> doctors;
+    Staff staff;
     string name, speciality;
-    int priority, duration;
+    set<string> specialities;
+    int priority, duration, arrival_time, no_specialities;
     
     inFile >> no_problems;
 
@@ -46,11 +69,12 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
+        inFile >> arrival_time;
         inFile >> duration;
         inFile >> priority;
 
         cases.emplace_back(name, speciality, priority, duration);
-        cout << name << ' ' << speciality << ' ' << priority << ' ' << duration << '\n';
+        cout << name << ' ' << speciality << ' ' << arrival_time << ' ' << priority << ' ' << duration << '\n';
     }
 
     cout << endl;
@@ -58,11 +82,21 @@ int main()
 
     for (int i = 0; i < no_doctors; i++)
     {
+        auto current_doctor = Doctor();
         inFile >> name;
-        inFile >> speciality;
+        inFile >> no_specialities;
+        cout << name << ' ' << no_specialities << ' ';
+        
+        current_doctor.name = name;
+        for (int i = 0; i < no_specialities; i++) {
+            inFile >> speciality;
+            current_doctor.specialities.insert(speciality);
+            specialities.insert(speciality);
+            staff.doctorsBySpecialities[speciality].emplace_back(current_doctor);
+            cout << speciality << '\n';
+        }
 
-        doctors.emplace_back(name, speciality);
-        cout << name << ' ' << speciality << '\n';
+        doctors.emplace_back(current_doctor);
     }
 
     sort(cases.begin(), cases.end(), [](const Case& c1, const Case& c2) {
